e717c8404899f8c664a9b87677bbb0d0e57169e0
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-10-24 16:24:31 +0300

    created models

---
+++ AlgebricEquationSystemSolver.DataAccess/Models/AlgebricEquationSystem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebricEquationSystemSolver.DataAccess.Models
+{
+	public class AlgebricEquationSystem
+	{
+		public Guid Id { get; set; }
+		public List<int>? Parameters { get; set; }
+	}
+}
---
+++ AlgebricEquationSystemSolver.DataAccess/Models/Configurations/AlgebricEquationSystemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebricEquationSystemSolver.DataAccess.Models.Configurations
+{
+	public class AlgebricEquationSystemConfiguration : IEntityTypeConfiguration<AlgebricEquationSystem>
+	{
+		public void Configure(EntityTypeBuilder<AlgebricEquationSystem> builder)
+		{
+			builder.HasKey(x => x.Id);
+		}
+	}
+}
